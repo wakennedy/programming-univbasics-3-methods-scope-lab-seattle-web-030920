919c75e2220563a427b48952a57e0ce2be23abee
William Kennedy <alexkenned@gmail.com>
2020-02-12 21:26:12 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
-phrase = "It's-a me, Mario!"
-status = "Thank You Mario! But Our Princess Is In Another Castle!"
+#phrase = "It's-a me, Mario!"
+#status = "Thank You Mario! But Our Princess Is In Another Castle!"
 
 
 def mario
@@ -8,5 +8,6 @@ def mario
 end
 
 def toadstool
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
