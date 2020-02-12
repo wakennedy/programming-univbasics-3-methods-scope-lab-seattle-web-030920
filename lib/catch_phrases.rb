e2dc5d31415576c7681669d7fe73029c129b1427
William Kennedy <alexkenned@gmail.com>
2020-02-12 21:20:41 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,8 @@
 phrase = "It's-a me, Mario!"
-
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
